af680b7f03c742116d2c0507921195b3d8bdea91
brogantrapp <31brogant@gmail.com>
2026-07-18 15:00:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+const startup = document.getElementById("startup");
+const app = document.getElementById("app");
 
+let started = false;
+
+
+// START WITH SPACE
+
+document.addEventListener("keydown", (event)=>{
+
+    if(event.code === "Space" && !started){
+
+        started = true;
+
+        startup.style.opacity = "0";
+
+        setTimeout(()=>{
+
+            startup.style.display="none";
+
+            app.style.opacity="1";
+
+        },1000);
+
+    }
+
+});
+
+
+// SIDEBAR
+
+const menu = document.getElementById("menu");
+const sidebar = document.getElementById("sidebar");
+
+
+menu.onclick = ()=>{
+
+    sidebar.classList.toggle("collapsed");
+
+};
